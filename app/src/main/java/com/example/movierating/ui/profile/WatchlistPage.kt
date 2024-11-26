bfd6b0e90b8b7e1e3cd706fd6f844b3f280498d9
seyeon <banabeast.kim@gmail.com>
2024-11-26 15:22:07 +0900

    보고싶어요 페이지 추가수정

--- app/src/main/java/com/example/movierating/ui/profile/WatchlistPage.kt
+++ app/src/main/java/com/example/movierating/ui/profile/WatchlistPage.kt
@@ -22,12 +22,14 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Close
 import androidx.compose.material.icons.outlined.Delete
 import androidx.compose.material.icons.outlined.Edit
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -59,21 +61,39 @@ fun WatchlistPage(){
     val filteredMovies = remember { mutableStateOf<List<Movie>>(emptyList()) }
     val isFilterApplied = remember { mutableStateOf(false) } // 필터 상태
     val isEditing = remember { mutableStateOf(false) } // 편집 모드 상태
+    val isDeleteDialogOpen = remember { mutableStateOf(false) } // 삭제 팝업 상태
+    val movieToDelete = remember { mutableStateOf<Movie?>(null) } // 삭제할 영화 정보
 
     // 필터 버튼 클릭 시 처리
     val onFilterClick = {
-        isFilterApplied.value = !isFilterApplied.value
-        filteredMovies.value = if (isFilterApplied.value) {
+        /*filteredMovies.value = if (isFilterApplied.value) {
             movies.value.filter { it.rating == null || it.rating == "" } // 평가되지 않은 영화 필터링
         } else {
             movies.value // 필터 해제 시 모든 영화 표시
+        }*/
+        if(isEditing.value){
+            isEditing.value = false
+            isFilterApplied.value = isFilterApplied.value
+        }
+        else{
+            isFilterApplied.value = !isFilterApplied.value
         }
     }
 
     // 수정 아이콘 클릭 시 처리
     val onEditClick = {
-        isEditing.value = !isEditing.value
+        if(isEditing.value){
+            isDeleteDialogOpen.value = true
+        }
+        else{
+            isEditing.value = true
+        }
     }
+
+    /*val onDeleteClick = { movie: Movie ->
+        movieToDelete.value = movie
+        isDeleteDialogOpen.value = true
+    }*/
 
     // Firestore 데이터 가져오기
     LaunchedEffect(Unit) {
@@ -85,14 +105,14 @@ fun WatchlistPage(){
     }
 
     Row(
-        modifier = Modifier.fillMaxWidth(),
+        modifier = Modifier.fillMaxWidth().padding(horizontal = 16.dp),
         verticalAlignment = Alignment.CenterVertically,
         horizontalArrangement = Arrangement.SpaceBetween
     ) {
         Button(
             onClick = onFilterClick,
             colors = ButtonDefaults.buttonColors(
-                containerColor = if(isFilterApplied.value) Color.Black else Color.LightGray
+                containerColor = if(isFilterApplied.value || isEditing.value) Color.Black else Color.LightGray
             ),
             modifier = Modifier
                 .width(160.dp)
@@ -100,7 +120,7 @@ fun WatchlistPage(){
                 .padding(vertical = 2.dp),
             contentPadding = PaddingValues(start = 16.dp, end = 16.dp)
         ) {
-            if (isFilterApplied.value) {
+            if (isFilterApplied.value || isEditing.value) {
                 Icon(
                     imageVector = Icons.Outlined.Close, // X 아이콘
                     contentDescription = "Close Filter",
@@ -110,8 +130,8 @@ fun WatchlistPage(){
             }
             Spacer(modifier = Modifier.width(8.dp))
             Text(
-                text = if(isFilterApplied.value)"편집취소" else "평가되지 않은 영화",
-                color = if(isFilterApplied.value)Color.White else Color.Black,
+                text = if(isEditing.value)"편집 취소" else "평가되지 않은 영화",
+                color = if(isFilterApplied.value || isEditing.value)Color.White else Color.Black,
                 fontSize = 12.sp
             )
         }
@@ -146,7 +166,7 @@ fun WatchlistPage(){
         Column(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(start = 16.dp, end = 16.dp, top = 2.dp),
+                .padding(start = 16.dp, end = 16.dp),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             Spacer(modifier = Modifier.height(8.dp))
@@ -154,7 +174,9 @@ fun WatchlistPage(){
             LazyVerticalGrid(
                 columns = GridCells.Fixed(3),
                 modifier = Modifier.fillMaxSize(),
-                contentPadding = PaddingValues(vertical = 8.dp)
+                contentPadding = PaddingValues(vertical = 8.dp),
+                verticalArrangement = Arrangement.spacedBy(16.dp), // 세로 간격 조정
+                horizontalArrangement = Arrangement.spacedBy(8.dp) // 가로 간격 조정
             ) {
                 items(movies.value) { movie ->
                     Column(
@@ -181,6 +203,46 @@ fun WatchlistPage(){
                 }
             }
         }
+        if (isDeleteDialogOpen.value) {
+            DeleteDialog(
+                onCancel = { isDeleteDialogOpen.value = false }, // 팝업 닫기
+                onDelete = {
+                    // 삭제 로직은 나중에 구현
+                    isDeleteDialogOpen.value = false
+                }
+            )
+        }
     }
 }
 
+@Composable
+fun DeleteDialog(
+    modifier: Modifier = Modifier,
+    onCancel: () -> Unit,
+    onDelete: () -> Unit
+){
+    AlertDialog(
+        onDismissRequest = {},
+        title = { Text("영화 삭제") },
+        text = { Text("선택한 영화가 보고싶어요 목록에서 삭제됩니다.") },
+        modifier = modifier,
+        confirmButton = {
+            Row(
+                modifier = Modifier.padding(horizontal = 6.dp)
+            ){
+                TextButton(
+                    onClick = onCancel,
+                    colors = ButtonDefaults.textButtonColors(contentColor = Color.Blue)
+                ) {
+                    Text(text = "취소")
+                }
+                TextButton(
+                    onClick = onDelete,
+                    colors = ButtonDefaults.textButtonColors(contentColor = Color.Red)
+                ) {
+                    Text(text = "삭제")
+                }
+            }
+        }
+    )
+}
