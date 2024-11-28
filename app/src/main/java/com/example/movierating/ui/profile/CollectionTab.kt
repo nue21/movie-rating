c70a731f6ac18c45c405b3c55ecdfe6f7562b8b4
nue21 <lmoow21@gmail.com>
2024-11-29 03:14:15 +0900

    file 정리

--- app/src/main/java/com/example/movierating/ui/profile/CollectionTab.kt
+++ app/src/main/java/com/example/movierating/ui/profile/CollectionTab.kt
@@ -1,5 +1,8 @@
 package com.example.movierating.ui.profile
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -8,13 +11,17 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Check
 import androidx.compose.material.icons.filled.Clear
@@ -23,11 +30,14 @@ import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextField
@@ -39,8 +49,12 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.example.movierating.data.Collection
 import com.example.movierating.data.Movie
@@ -220,6 +234,81 @@ fun CollectionTabContent(navController: NavController) {
                 }
             }
 
+        }
+    }
+}
+
+
+@Composable
+fun CollectionCard(
+    movies: List<Movie>,
+    isSelected: Boolean,
+    onClick: () -> Unit,
+    isEdit: Boolean,
+) {
+    var title = remember { mutableStateOf("컬렉션") }
+    Card(
+        modifier = Modifier
+            .width(100.dp)
+            .padding(8.dp)
+            .clickable { onClick() }
+            .border(
+                width = if (isSelected) 3.dp else 0.dp, // 선택 여부에 따라 테두리 두께 변경
+                color = if (isSelected) Color.Gray else Color.White, // 선택 여부에 따라 색상 변경
+                shape = RoundedCornerShape(12.dp)
+            ),
+        shape = RoundedCornerShape(12.dp),
+        colors = CardDefaults.cardColors(Color(0xFFF9F9F9))
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(8.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            // 영화 포스터 섹션
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                contentAlignment = Alignment.Center
+            ) {
+                // movies가 비어있는 경우 대체 UI 출력
+                if (movies.isNotEmpty() && movies[0].posters != null) {
+                    movies[0].posters?.let { MovieImage(it) }
+                } else {
+                    Box(
+                        modifier = Modifier
+                            .size(width = 80.dp, height = 120.dp) // 고정 크기 설정
+                            .clip(RoundedCornerShape(8.dp))
+                            .background(Color.LightGray), // 회색 배경
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Text(
+                            text = "No Image",
+                            color = Color.DarkGray,
+                            style = MaterialTheme.typography.bodyMedium
+                        )
+                    }
+                }
+            }
+            Spacer(modifier = Modifier.height(8.dp))
+            // 컬렉션 제목
+            if (!isEdit) {
+                Text(
+                    text = title.value,
+                    style = MaterialTheme.typography.titleSmall.copy(fontWeight = FontWeight.Bold),
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis
+                )
+            }
+            else {
+                TextField(
+                    value = title.value,
+                    onValueChange = {title.value = it},
+                    modifier = Modifier.width(100.dp).height(56.dp),
+                    textStyle = androidx.compose.ui.text.TextStyle(fontSize = 12.sp),
+                    placeholder = {Text(text = title.value)}
+                )
+            }
         }
     }
 }
